9d464e2b7f4273b5bec8de8e4368c11d4192a561
Rudra Tailor <rudratailor1836@outlook.com>
2025-07-25 12:30:03 +0000

    Added Trie

---
+++ Trie/Trie.py
@@ -0,0 +1,28 @@
+class Node:
+
+    def __init__(self):
+        self.children = dict()
+        self.end_of_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+    
+    def insert(self, word):
+        pass
+
+    def search(self, word):
+        pass
+
+    def delete(self, word):
+        pass
+
+    def has_prefix(self, prefix):
+        pass
+
+    def starts_with(self, prefix):
+        pass
+
+    def lists_word(self):
+        pass
